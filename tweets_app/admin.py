739f7ba8445a197c57ca1388aae320710fd331e5
KolbotBou <kolbot.bou@gmail.com>
2024-11-27 17:00:05 +0700

    Add some icons and UI to Base Homepage

--- tweets_app/admin.py
+++ tweets_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tweets_app.models import UserAccount, Post, PostComment
 
 # Register your models here.
+admin.site.register(UserAccount)
+admin.site.register(Post)
+admin.site.register(PostComment)
